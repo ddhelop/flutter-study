a711d69deafd0d1c9f01c349de09177c53b7260b
김동혁 <clfgnm9@naver.com>
2025-07-19 17:21:21 +0900

    앱바 및 플로팅 액션 버튼 추가, 텍스트 크기 변경

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,32 @@ import 'package:flutter/material.dart';
 void main() {
   // MaterialApp : 가장 기본이 되는 위젯트리의 최상위에 위치
   // Scaffold : 화면의 기본 구조를 담당(기본 화면 가장 쉽게 만듬)
-  runApp(MaterialApp(home: Scaffold(body: TestWidget())));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {
+                print('Tab!');
+              },
+            ),
+            IconButton(icon: Icon(Icons.play_arrow), onPressed: () {}),
+          ],
+          centerTitle: false,
+          title: Text('This is AppBar'),
+        ),
+        body: TestWidget(),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.bug_report),
+          onPressed: () {
+            print('Floating Action Button!');
+          },
+        ),
+      ),
+    ),
+  );
 }
 
 class TestWidget extends StatelessWidget {
@@ -15,7 +40,7 @@ class TestWidget extends StatelessWidget {
       child: Center(
         child: Text(
           'Hello, Flutter',
-          style: TextStyle(fontSize: 40, color: Colors.black),
+          style: TextStyle(fontSize: 60, color: Colors.black),
         ),
       ),
     );
